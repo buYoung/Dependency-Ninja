a9b7597eca8a7cd0dada830fe536a89fece85d58
GitHub Action <action@github.com>
2026-02-26 08:34:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/buyoung/dependencyninja/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/buyoung/dependencyninja/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.buyoung.dependencyninja.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
